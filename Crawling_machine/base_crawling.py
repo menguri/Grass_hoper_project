66528757f6747613d5e54dcbaadb425a6d0647ac
menguri <tatalintelli@gmail.com>
2022-02-21 12:48:38 +0900

    2022.02.21

--- Crawling_machine/base_crawling.py
+++ Crawling_machine/base_crawling.py
@@ -22,216 +22,6 @@ from bs4 import BeautifulSoup
 from html_table_parser import parser_functions as parser
 
 
-# Every Day, Crawling investors trend
-# step 1. Daily 수집
-
-def Daily_VolumeData(corporation, num, driver, time, dates):
-    week = datetime.date(time[0], time[1], time[2]).weekday()
-    if week == 5 :
-        return print("Today is weekend. No Data.")
-    elif week == 6 :
-        return print("Today is weekend. No Data.")
-    # 검색창 입력
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="btnisuCd_finder_stkisu0_0"]'))).click()
-    sleep(2)
-    search_cor = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="searchText__finder_stkisu0_0"]')))
-    search_cor.send_keys(corporation)
-    search_cor.send_keys(Keys.RETURN)
-    sleep(3)
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="jsGrid__finder_stkisu0_0"]/tbody/tr[1]/td[1]'))).click()
-    sleep(2)
-    driver.find_element_by_xpath('//*[@id="jsSearchButton"]').click()
-    sleep(8)
-    # ----- Data Crawling -----
-    Price = driver.find_element_by_xpath('//*[@id="isuInfoBind"]/table/tbody/tr[1]/td[1]').text
-    Count = driver.find_element_by_xpath('//*[@id="isuInfoBind"]/table/tbody/tr[1]/td[2]').text
-    Foreign_rate = driver.find_element_by_xpath('//*[@id="isuInfoBind"]/table/tbody/tr[4]/td[2]').text
-    PER_PBR = driver.find_element_by_xpath('//*[@id="isuInfoBind"]/table/tbody/tr[5]/td[2]').text
-    PER, PBR = PER_PBR.split('/')
-    # Price = int(re.sub(r"[^a-zA-Z0-9]","", Price))
-    # Count = int(re.sub(r"[^a-zA-Z0-9]","", Count))
-    # 이동
-    if num == 0:
-        driver.find_element_by_xpath('//*[@id="jsMdiMenu"]/div[4]/ul/li[1]/ul/li[2]/div/div[1]/ul/li[2]/ul/li[3]/a').send_keys(Keys.ENTER)
-    sleep(1)
-    driver.find_element_by_xpath('//*[@id="jsMdiMenu"]/div[4]/ul/li[1]/ul/li[2]/div/div[1]/ul/li[2]/ul/li[3]/ul/li[2]/a').send_keys(Keys.ENTER)
-    # ----- 거래내역 뽑기 -----
-    # 검색창 입력
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/section[2]/section/section/div/div[2]/form/div[1]/div/table/tbody/tr[2]/td/div/div/p/img'))).click()
-    search_cor = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="searchText__finder_stkisu0_1"]')))
-    search_cor.clear()
-    search_cor.send_keys(corporation)
-    search_cor.send_keys(Keys.RETURN)
-    sleep(3)
-    driver.find_element_by_xpath('/html/body/div[2]/section[2]/section/section/div/div[2]/div[7]/div[2]/div/form/div[2]/a').click()
-    sleep(2)
-    driver.find_element_by_xpath('/html/body/div[2]/section[2]/section/section/div/div[2]/div[7]/div[2]/div/div/div/div/div/div/div[2]/table/tbody/tr[1]/td[2]').click()
-    sleep(2)
-    # 날짜 입력
-    start = driver.find_element_by_xpath('//*[@id="strtDd"]')
-    start.clear()
-    start.send_keys(dates)
-    sleep(1)
-    end = driver.find_element_by_xpath('//*[@id="endDd"]')
-    end.clear()
-    end.send_keys(dates)
-    sleep(1)
-    driver.find_element_by_xpath('/html/body/div[2]/section[2]/section/section/div/div[2]/form/div[1]/div/a').click()
-    sleep(4)
-    html = driver.page_source
-    # pip install lxml
-    soup = BeautifulSoup(html, 'html.parser')
-    table_html = soup.find('table', {'class' : 'CI-GRID-BODY-TABLE'})
-    table_html = str(table_html)
-    table_df_list = pd.read_html(table_html)
-    table_df = table_df_list[0]
-    Corpor_count = str(table_df['순매수'][7])
-    Personal_count = str(table_df['순매수'][9])
-    Foreign_count = str(table_df['순매수'][10])
-    # 되돌아가기
-    driver.find_element_by_xpath('//*[@id="jsMdiMenu"]/div[4]/ul/li[1]/ul/li[2]/div/div[1]/ul/li[2]/ul/li[2]/ul/li[3]/a').send_keys(Keys.ENTER)
-    # fin
-    dic = {
-        'Price': Price,
-        'Count': Count,
-        'Foreign_rate': Foreign_rate,
-        'PER': PER, 
-        'PBR': PBR, 
-        'Corpor_count': Corpor_count, 
-        'Personal_count': Personal_count, 
-        'Foreign_count': Foreign_count,
-        'week': str(week)
-    }
-    # crawling_df = pd.DataFrame(dic, index=[num])
-    Count_storage(dic, corporation, dates)
-
-
-
-# step 2. 원하는 기간만큼 데이터 수집
-
-def Period_VolumeData(corporation, num, driver, week, dates):
-    # 기업 바뀔 때마다 새로고침
-    # if num == 0 :
-    #     driver.refresh()
-    #     print('refresh is completed')
-    # [투자자별 실적 거래] 이동  --------------------------------------------------------------------------------------------------------------------
-    sleep(5)
-    if num == 0:
-        driver.find_element_by_xpath('/html/body/div[2]/section[2]/aside/div[4]/ul/li[1]/ul/li[2]/div/div[1]/ul/li[2]/ul/li[3]/a').send_keys(Keys.ENTER)
-    sleep(2)
-    driver.find_element_by_xpath('/html/body/div[2]/section[2]/aside/div[4]/ul/li[1]/ul/li[2]/div/div[1]/ul/li[2]/ul/li[3]/ul/li[2]/a').send_keys(Keys.ENTER)
-    # ----- 거래내역 뽑기 -----
-    # 검색창 입력
-    if num == 0:
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/section[2]/section/section/div/div[2]/form/div[1]/div/table/tbody/tr[2]/td/div/div/p/img'))).click()
-        search_cor = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/section[2]/section/section/div/div[2]/div[7]/div[2]/div/form/div[2]/input[1]')))
-        search_cor.clear()
-        search_cor.send_keys(corporation)
-        search_cor.send_keys(Keys.RETURN)
-        sleep(3)
-        driver.find_element_by_xpath('/html/body/div[2]/section[2]/section/section/div/div[2]/div[7]/div[2]/div/form/div[2]/a').click()
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/section[2]/section/section/div/div[2]/div[7]/div[2]/div/div/div/div/div/div/div[2]/table/tbody/tr[1]/td[2]'))).click()
-    sleep(2)
-    # 날짜 입력
-    start = driver.find_element_by_xpath('//*[@id="strtDd"]')
-    start.clear()
-    start.send_keys(dates)
-    end = driver.find_element_by_xpath('//*[@id="endDd"]')
-    end.clear()
-    end.send_keys(dates)
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/section[2]/section/section/div/div[2]/form/div[1]/div/a'))).click()
-    sleep(4)
-    html = driver.page_source
-    # pip install lxml
-    soup = BeautifulSoup(html, 'html.parser')
-    table_html = soup.find('table', {'class' : 'CI-GRID-BODY-TABLE'})
-    table_html = str(table_html)
-    table_df_list = pd.read_html(table_html)
-    table_df = table_df_list[0]
-    print(table_df)
-    Count = str(table_df['매수'][12])
-    Corpor_count = str(table_df['순매수'][7])
-    Personal_count = str(table_df['순매수'][9])
-    Foreign_count = str(table_df['순매수'][10])
-    # [PER/PBR 개별 종목] 이동  --------------------------------------------------------------------------------------------------------------------
-    if num == 0:
-        driver.find_element_by_xpath('/html/body/div[2]/section[2]/aside/div[4]/ul/li[1]/ul/li[2]/div/div[1]/ul/li[2]/ul/li[5]/a').send_keys(Keys.ENTER)
-    sleep(1)
-    driver.find_element_by_xpath('/html/body/div[2]/section[2]/aside/div[4]/ul/li[1]/ul/li[2]/div/div[1]/ul/li[2]/ul/li[5]/ul/li[2]/a').send_keys(Keys.ENTER)
-    sleep(2)
-    driver.find_element_by_xpath('//*[@id="MDCSTAT035_FORM"]/div[2]/div/table/tbody/tr[1]/td/label[2]').click()
-    sleep(2)
-    # 이전 창들 제거
-    driver.find_element_by_xpath('//*[@id="jsMdiTab"]/li[2]/a/button').click()
-    driver.find_element_by_xpath('//*[@id="jsMdiTab"]/li[1]/a/button').click()
-    # 날짜 입력
-    start = driver.find_element_by_xpath('//*[@id="startCalender"]')
-    start.clear()
-    start.send_keys(dates)
-    sleep(1)
-    end = driver.find_element_by_xpath('//*[@id="endCalendar"]')
-    end.clear()
-    end.send_keys(dates)
-    driver.find_element_by_xpath('//*[@id="jsSearchButton"]').click()
-    sleep(4)
-    per_html = driver.page_source
-    soup = BeautifulSoup(per_html, 'html.parser')
-    table_html = soup.find_all('table', {'class' : 'CI-GRID-BODY-TABLE'})
-    table_html = str(table_html)
-    table_df_list = pd.read_html(table_html)
-    table_df = table_df_list[1]
-    print(table_df)
-    PER = str(table_df['PER'][0])
-    PBR = str(table_df['PBR'][0])
-    Price = str(table_df['종가'][0])
-    # [외국인 보유량 개별종목] 이동  --------------------------------------------------------------------------------------------------------------------
-    driver.find_element_by_xpath('/html/body/div[2]/section[2]/aside/div[4]/ul/li[1]/ul/li[2]/div/div[1]/ul/li[2]/ul/li[5]/ul/li[4]/a').send_keys(Keys.ENTER)
-    sleep(1)
-    # 이전 창 삭제
-    driver.find_element_by_xpath('//*[@id="jsMdiTab"]/li[1]/a/button').click()
-    # 검색 시작
-    driver.find_element_by_xpath('//*[@id="MDCSTAT037_FORM"]/div[2]/div/table/tbody/tr[1]/td/label[2]').click()
-    sleep(2)
-    # 날짜 입력
-    start = driver.find_element_by_xpath('//*[@id="startCalender"]')
-    start.clear()
-    start.send_keys(dates)
-    sleep(1)
-    end = driver.find_element_by_xpath('//*[@id="endCalendar"]')
-    end.clear()
-    end.send_keys(dates)
-    driver.find_element_by_xpath('//*[@id="jsSearchButton"]').click()
-    sleep(4)
-    foreign_html = driver.page_source
-    soup = BeautifulSoup(foreign_html, 'html.parser')
-    table_html = soup.find_all('table', {'class' : 'CI-GRID-BODY-TABLE'})
-    table_html = str(table_html)
-    table_df_list = pd.read_html(table_html)
-    table_df = table_df_list[1]
-    print(table_df)
-    Foreign_rate = str(table_df['외국인지분율'][0])
-    # 되돌아가기
-    driver.find_element_by_xpath('//*[@id="jsMdiMenu"]/div[4]/ul/li[1]/ul/li[2]/div/div[1]/ul/li[2]/ul/li[2]/ul/li[3]/a').send_keys(Keys.ENTER)
-    sleep(2)
-    # 이전 창 삭제
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/section[2]/section/ul/li[1]/a/button'))).click()
-    # fin
-    dic = {
-        'Price': Price,
-        'Count': Count,
-        'Foreign_rate': Foreign_rate,
-        'PER': PER, 
-        'PBR': PBR, 
-        'Corpor_count': Corpor_count, 
-        'Personal_count': Personal_count, 
-        'Foreign_count': Foreign_count,
-        'week': str(week)
-    }
-    # crawling_df = pd.DataFrame(dic, index=[num])
-    Count_storage(dic, corporation, dates)
-
-
-
 
 class crawling_corporation:
     def __init__(self) -> None:
@@ -258,17 +48,11 @@ class crawling_corporation:
             WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/section[2]/section/section/div/div[2]/div[7]/div[2]/div/div/div/div/div/div/div[2]/table/tbody/tr[1]/td[2]'))).click()
         sleep(2)
         # 날짜 입력
-        start = driver.find_element_by_xpath('//*[@id="strtDd"]')
-        for i in range(0,2):
-            start.clear()
-            start.send_keys(dates)
-            sleep(1)
-            print(start.get_attribute('value'))
-        end = driver.find_element_by_xpath('//*[@id="endDd"]')
-        for i in range(0,2):
-            end.clear()
-            end.send_keys(dates)
-            sleep(1)
+        end = driver.find_element_by_xpath('/html/body/div[2]/section[2]/section/section/div/div[2]/form/div[1]/div/table/tbody/tr[3]/td[1]/div/div/input[2]')
+        end.clear()
+        end.send_keys(dates)
+        sleep(1)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/section[2]/section/section/div/div[2]/form/div[1]/div/table/tbody/tr[3]/td[1]/div/div/button[2]'))).click()
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/section[2]/section/section/div/div[2]/form/div[1]/div/a'))).click()
         sleep(4)
         html = driver.page_source
@@ -278,7 +62,6 @@ class crawling_corporation:
         table_html = str(table_html)
         table_df_list = pd.read_html(table_html)
         table_df = table_df_list[0]
-        print(table_df)
         Count = str(table_df['매수'][12])
         Corpor_count = str(table_df['순매수'][7])
         Personal_count = str(table_df['순매수'][9])
@@ -315,14 +98,11 @@ class crawling_corporation:
             WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/section[2]/section/section/div/div[2]/div[4]/div[2]/div/div/div/div/div/div/div[2]/table/tbody/tr[1]/td[2]'))).click()
         sleep(2)
         # 날짜 입력
-        start = driver.find_element_by_xpath('//*[@id="startCalender"]')
-        for i in range(0,2):
-            start.clear()
-            start.send_keys(dates)
         end = driver.find_element_by_xpath('//*[@id="endCalendar"]')
-        for i in range(0,2):
-            end.clear()
-            end.send_keys(dates)
+        end.clear()
+        end.send_keys(dates)
+        sleep(1)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/section[2]/section/section/div/div[2]/form/div[2]/div/table/tbody/tr[5]/td/div/div/button[2]'))).click()
         driver.find_element_by_xpath('/html/body/div[2]/section[2]/section/section/div/div[2]/form/div[2]/div/a').click()
         sleep(4)
         per_html = driver.page_source
@@ -331,7 +111,6 @@ class crawling_corporation:
         table_html = str(table_html)
         table_df_list = pd.read_html(table_html)
         table_df = table_df_list[1]
-        print(table_df)
         PER = str(table_df['PER'][0])
         PBR = str(table_df['PBR'][0])
         Price = str(table_df['종가'][0]) 
@@ -365,23 +144,20 @@ class crawling_corporation:
             WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/section[2]/section/section/div/div[2]/div[4]/div[2]/div/div/div/div/div/div/div[2]/table/tbody/tr[1]/td[2]'))).click()
         sleep(2)
         # 날짜 입력
-        start = driver.find_element_by_xpath('//*[@id="startCalender"]')
-        for i in range(0,2):
-            start.clear()
-            start.send_keys(dates)
         end = driver.find_element_by_xpath('//*[@id="endCalendar"]')
-        for i in range(0,2):
-            end.clear()
-            end.send_keys(dates)
+        end.clear()
+        end.send_keys(dates)
+        sleep(1)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/section[2]/section/section/div/div[2]/form/div[2]/div/table/tbody/tr[5]/td/div/div/button[2]'))).click()
         driver.find_element_by_xpath('/html/body/div[2]/section[2]/section/section/div/div[2]/form/div[2]/div/a').click()
         sleep(4)
+        # 페이지 크롤링
         foreign_html = driver.page_source
         soup = BeautifulSoup(foreign_html, 'html.parser')
         table_html = soup.find_all('table', {'class' : 'CI-GRID-BODY-TABLE'})
         table_html = str(table_html)
         table_df_list = pd.read_html(table_html)
         table_df = table_df_list[1]
-        print(table_df)
         Foreign_rate = str(table_df['외국인지분율'][0])
         # fin
         dic = {
--- Crawling_machine/body.py
+++ Crawling_machine/body.py
@@ -1,30 +1,29 @@
-from base_crawling import Daily_VolumeData, Period_VolumeData, crawling_corporation
+from turtle import pd
+from base_crawling import crawling_corporation
 from firebase import Check_data
 from selenium.webdriver.chrome.options import Options
 import selenium
 from selenium import webdriver
 from time import sleep
 from datetime import datetime, timedelta, date
+import pandas as pd
+from firebase import Count_storage
+import time
+import datetime
+import math
 
 # input 기업 입력
 cor = []
-# corporation_num = input("원하는 기업의 수를 입력하세요 : ")
-corporation_num = 2
-# for i in range(0, int(corporation_num)):
-#     corporation = str(input(f"원하는 기업을 입력하세요 ({i+1}/{int(corporation_num)}) : "))
-#     cor.append(corporation)
-cor = ['삼성전자', '카카오']
+corporation_num = input("원하는 기업의 수를 입력하세요 : ")
+for i in range(0, int(corporation_num)):
+    corporation = str(input(f"원하는 기업을 입력하세요 ({i+1}/{int(corporation_num)}) : "))
+    cor.append(corporation)
 
 # time 선택
-# start = str(input("시작 시점을 입력하세요(주말X). ex) 2021-08-01 : "))
-# finish = str(input("종료 시점을 입력하세요. ex) 2021-08-01 : "))
-start = '2022-02-08'
-finish = '2022-02-09'
+start = str(input("시작 시점을 입력하세요(주말X). ex) 2021-08-01 : "))
+finish = str(input("종료 시점을 입력하세요. ex) 2021-08-01 : "))
 start_date = datetime.strptime(start, "%Y-%m-%d") 
 last_date = datetime.strptime(finish, "%Y-%m-%d") 
-
-# mode 선택
-# mode = str(input("Daily or Period : "))
 
 # 갑자기 안될 때는 크롬 버전이 달라서 그런 것이므로, 그에 맞는 driver 다운로드하면 된다.
 options = webdriver.ChromeOptions()
@@ -33,76 +32,74 @@ options.add_argument("--no-sandbox")
 options.add_argument("--disable-setuid-sandbox")
 options.add_argument("start-maximized")
 options.add_argument("--disable-software-rasterizer")
-# options.add_argument('headless')
-# options.add_argument('window-size=1920x1080')
-# options.add_argument("disable-gpu")
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
 driver = webdriver.Chrome(executable_path='C:\chromedriver.exe', chrome_options=options)
 URL = 'http://data.krx.co.kr/contents/MDC/MDI/mdiLoader/index.cmd?menuId=MDC0201020203#'
 
+
+
 # Crawling 진행
-
-# # Crawling 시작
-# for i in cor:
-#     num = 0
-#     start = start_date
-#     last = last_date
-#     while start <= last: 
-#         dates = start.strftime("%Y%m%d") 
-#         time = [int(dates[0:4]), int(dates[4:6]), int(dates[6:8])]
-#         week = date(time[0], time[1], time[2]).weekday()
-#         # 중간 쉬어주기
-#         if num == 10:
-#             driver.close()
-#             sleep(10)
-#             driver = webdriver.Chrome(executable_path='C:\chromedriver', chrome_options=options)
-#             URL = 'http://data.krx.co.kr/contents/MDC/MDI/mdiLoader/index.cmd?menuId=MDC0201020203#'
-#             driver.get(url=URL)
-#             num = 0
-#         # weekend 일 경우, 뛰어넘어버려
-#         if week == 5 and 6 :
-#             print("Today is weekend. No Data.")
-#         else:
-#             if mode == 'Period':
-#                 Period_VolumeData(i, num, driver, week, dates)
-#                 num += 1
-#             else:
-#                 Daily_VolumeData(i, num, driver, week, dates)
-#                 num += 1
-#         start += timedelta(days=1)
-
-
+# 시간 측정
+start_time = time.time() 
+math.factorial(1234567) 
+# 시작
 crawl_corporation = crawling_corporation()
 for corporation in cor:
     print(f'----------------------Corporation is {corporation}----------------------')
+    # Dataframe
+    step1_df = pd.DataFrame()
+    step2_df = pd.DataFrame()
+    step3_df = pd.DataFrame()
     for step_number in ['step1', 'step2', 'step3']:
         driver = webdriver.Chrome(executable_path='C:\chromedriver.exe', chrome_options=options)
         driver.get(url=URL)
         start = start_date
         last = last_date
         num = 0
+        # 시간 반복
         while start <= last: 
             dates = start.strftime("%Y%m%d") 
             time = [int(dates[0:4]), int(dates[4:6]), int(dates[6:8])]
             week = date(time[0], time[1], time[2]).weekday()   
             # weekend 일 경우
-            if week == 5 and 6 :
+            if week in [5,6] :
                 print("Today is weekend. No Data.")
             else:
                 if step_number == 'step1':
                     dic = crawl_corporation.step1(corporation, driver, num, week, dates)
-                    print(dic)
+                    step1_dic = pd.DataFrame([dic])
+                    step1_df = pd.concat([step1_df, step1_dic])
                     num += 1
                 elif step_number == 'step2':
                     dic = crawl_corporation.step2(corporation, driver, num, week, dates)
-                    print(dic)
+                    step2_dic = pd.DataFrame([dic])
+                    step2_df = pd.concat([step2_df, step2_dic])
                     num += 1                   
                 else:
                     dic = crawl_corporation.step3(corporation, driver, num, week, dates)
-                    print(dic)
+                    step3_dic = pd.DataFrame([dic])
+                    step3_df = pd.concat([step3_df, step3_dic])
                     num += 1
             start += timedelta(days=1)
         driver.close()
         print(f'----------------------{step_number} is finished----------------------')
+    # final dataframe 정리
+    final_df = pd.merge(left = step1_df , right = step2_df, how = "inner", on = "Date")
+    final_df = pd.merge(left = final_df , right = step3_df, how = "inner", on = "Date")
+    row = final_df['Date'].copy()
+    for row_dict in final_df.to_dict(orient="records"):
+        time = row_dict['Date']
+        Count_storage(row_dict, corporation, time)
+# 코드 종료
+end_time = time.time()
+sec = (end_time - start_time) 
+result = datetime.timedelta(seconds=sec) 
+print(f'Start Time : {start_time}')
+print(f'End Time : {end_time}')
+print(f"Running Time : {result}")
+
 sleep(10)
 driver.quit()
 print('--------------------------------------------Crawling is completed--------------------------------------------')
